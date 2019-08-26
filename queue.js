beb79e7e8b1ec7148a71477a4903a182d95d0151
savinaxu <savinajia@gmail.com>
2019-08-25 23:48:36 -0700

    queue

--- queue.js
+++ queue.js
@@ -6,41 +6,89 @@ class Node {
     }
 }
 
-class Queue {
+// Implementaion with array
+class QueueArr {
     constructor() {
-        this.first = null
-        this.last = null
-        this.length = 0
+        this.arr = []
     }
 
     peek() {
-        return this.first
+        return this.arr[0]
     }
 
     enqueue(value) {
-        const newNode = new Node(value)
-        this.length === 0 ? this.first = newNode : this.last.next = newNode
-        this.last = newNode
-        this.length++
+        this.arr.push(value)
         return this
     }
 
     dequeue() {
-        if (!this.first) return null
-        if (this.first === this.last) this.last = null
-        this.first = this.first.next
-        this.length--
+        this.arr.shift()
         return this
     }
 }
 
-const myQueue = new Queue();
-myQueue.peek();
-myQueue.enqueue('Joy');
-myQueue.enqueue('Matt');
-myQueue.enqueue('Pavel');
-myQueue.peek();
-myQueue.dequeue();
-myQueue.dequeue();
-myQueue.dequeue();
-myQueue.peek();
+// Implementation with two stacks
+class Queue2Stacks {
+    constructor() {
+        this.first = [];
+        this.last = [];
+    }
+
+    enqueue(value) {
+        const length = this.first.length;
+        for (let i = 0; i < length; i++) {
+            this.last.push(this.first.pop());
+        }
+        this.last.push(value);
+        return this;
+    }
+
+    dequeue() {
+        const length = this.last.length;
+        for (let i = 0; i < length; i++) {
+            this.first.push(this.last.pop());
+        }
+        this.first.pop();
+        return this;
+    }
+    
+    peek() {
+        if (this.last.length > 0) {
+            return this.last[0];
+        }
+        return this.first[this.first.length - 1];
+    }
+}
+
+
+
+
+const myQueueLl = new QueueLinkedList();
+myQueueLl.enqueue('Joy');
+myQueueLl.enqueue('Matt');
+myQueueLl.enqueue('Pavel');
+myQueueLl.peek();
+myQueueLl.dequeue();
+myQueueLl.dequeue();
+myQueueLl.dequeue();
+myQueueLl.peek();
+
+const myQueueArr = new QueueArr();
+myQueueArr.enqueue('Joy');
+myQueueArr.enqueue('Matt');
+myQueueArr.enqueue('Pavel');
+myQueueArr.peek();
+myQueueArr.dequeue();
+myQueueArr.dequeue();
+myQueueArr.dequeue();
+myQueueArr.peek();
+
+const myQueue2Stack = new Queue2Stacks()
+myQueue2Stack.enqueue('Joy');
+myQueue2Stack.enqueue('Matt');
+myQueue2Stack.enqueue('Pavel');
+myQueue2Stack.peek();
+myQueue2Stack.dequeue();
+myQueue2Stack.dequeue();
+myQueue2Stack.dequeue();
+myQueue2Stack.peek();
